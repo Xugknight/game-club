a6f0d3ffd3a3b6812965de40b83d21727e3dbc2d
timothyn <tnguyen_42@yahoo.com>
2025-06-09 16:31:47 -0400

    Fixing games.js controller and refactoring more

--- backend/controllers/games.js
+++ backend/controllers/games.js
@@ -7,24 +7,35 @@ module.exports = {
 
 async function index(req, res) {
   try {
-    const posts = await Post.find({});
+    const games = await Game.find({});
     // Below would return all posts for just the logged in user
     // const posts = await Post.find({author: req.user._id});
-    res.json(posts);
+    res.json(games);
   } catch (err) {
     console.log(err);
-    res.status(500).json({ message: 'Failed to fetch posts' });
+    res.status(500).json({ message: 'Failed to Fetch Games' });
+  }
+}
+
+async function show(req, res) {
+  try {
+    const game = await Game.findById(req.params.gameId).populate('createdBy', 'username');
+    if (!game) return res.status(404).json({ message: 'Game not found' });
+    res.json(game);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ message: 'Failed to Fetch Game' });
   }
 }
 
 async function create(req, res) {
   try {
     req.body.createdBy = req.user._id;
-    const post = await Post.create(req.body);
-    res.json(post);
+    const game = await Game.create(req.body);
+    res.json(game);
   } catch (err) {
     console.log(err);
-    res.status(400).json({ message: 'Failed to creat post' });
+    res.status(400).json({ message: 'Failed to Create Game' });
   }
 }
 
