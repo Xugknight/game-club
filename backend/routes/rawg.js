a26c94a4f57ebe6c3f82da15f49d1e1f1566306d
timothyn <tnguyen_42@yahoo.com>
2025-06-10 17:07:48 -0400

    create rawg.js route, and add rawg API key to .env

---
+++ backend/routes/rawg.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/rawg/games', async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+});
