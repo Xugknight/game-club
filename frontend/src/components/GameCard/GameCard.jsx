923705692f22400175ab2fda15446c9c16af2abe
timothyn <tnguyen_42@yahoo.com>
2025-06-11 17:45:50 -0400

    Create GameCard component and add to GameListPage. Add some styling for card as well, will probably style more later once I start styling everything.

---
+++ frontend/src/components/GameCard/GameCard.jsx
@@ -0,0 +1,22 @@
+import { Link } from 'react-router';
+import './GameCard.css';
+
+export default function GameCard({ game }) {
+    return (
+        <div className='game-card'>
+            <Link to={`/games/${game._id}`} className='game-card__link'>
+                <img 
+                    src="game.coverImageUrl" 
+                    alt={`Cover image for ${game.title}`} 
+                    className='game-card__image'
+                />
+                <div className='game-card__info'>
+                    <h3 className='game-card__title'>{game.title}</h3>
+                    <p>
+                        {game.developer} - {new Date(game.releaseDate).getFullYear()}
+                    </p>
+                </div>
+            </Link>
+        </div>
+    );
+};
--- frontend/src/pages/GameListPage/GameListPage.jsx
+++ frontend/src/pages/GameListPage/GameListPage.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import * as gameService from '../../services/gameService';
+import GameCard from '../../components/GameCard/GameCard';
 
 export default function GameListPage() {
   const [games, setGames] = useState([]);
@@ -16,9 +17,11 @@ export default function GameListPage() {
     <>
       <h1>Game List</h1>
       {games.length ? 
-        <ul>
-          {games.map((game) => <li key={game._id}>{game.title}</li>)}
-        </ul>
+        <div className='game-grid'>
+            {games.map((game) => 
+              <GameCard key={game._id} game={game} />
+            )}
+        </div>
         :
         <p>No Games Yet!</p>
       }
