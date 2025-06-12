91e44e7db16908a95260dd29a13a40f7b0a09d1f
timothyn <tnguyen_42@yahoo.com>
2025-06-12 13:29:23 -0400

    Set up reviewService.js

--- frontend/src/services/reviewService.js
+++ frontend/src/services/reviewService.js
@@ -2,10 +2,22 @@ import sendRequest from "./sendRequest";
 
 const BASE_URL = '/api/games';
 
-export function Reviews() {};
+export function getReviews(gameId) {
+    // GET /api/games/:gameId/reviews
+    return sendRequest(`${BASE_URL}/${gameId}/reviews`);
+};
 
-export function Reviews() {};
+export function createReview(gameId, data) {
+    // POST /api/games/:gameId/reviews
+    return sendRequest(`${BASE_URL}/${gameId}/reviews`, 'POST', data);
+};
 
-export function Reviews() {};
+export function updateReview(gameId, reviewId, data) {
+    // PUT /api/games/:gameId/reviews/:reviewId
+    return sendRequest(`${BASE_URL}/${gameId}/reviews/${reviewId}`, 'PUT', data);
+};
 
-export function Reviews() {};
+export function deleteReview(gameId, reviewId,) {
+    // DELETE /api/games/:gameId/reviews/:reviewId
+    return sendRequest(`${BASE_URL}/${gameId}/reviews/${reviewId}`, 'DELETE');
+};
